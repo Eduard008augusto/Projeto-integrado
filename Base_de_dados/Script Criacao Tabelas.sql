118f0e1d2d562541781fbea69e3dd40f5a77df74
tugafirept <tugafirept@gmail.com>
2024-04-12 15:28:03 +0100

    Nova versao bd

--- Base_de_dados/Script Criacao Tabelas.sql
+++ Base_de_dados/Script Criacao Tabelas.sql
@@ -1,8 +1,7 @@
 /*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2022                    */
-/* Created on:     03/04/2024 11:25:59                          */
+/* Created on:     12/04/2024 14:42:25                          */
 /*==============================================================*/
-
 
 if exists (select 1
             from  sysobjects
@@ -34,9 +33,9 @@ go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('FOTOGRAFIAS')
+           where  id = object_id('FOTOGRAFIAS_CONTEUDO')
             and   type = 'U')
-   drop table FOTOGRAFIAS
+   drop table FOTOGRAFIAS_CONTEUDO
 go
 
 if exists (select 1
@@ -44,6 +43,13 @@ if exists (select 1
            where  id = object_id('FAVORITOS')
             and   type = 'U')
    drop table FAVORITOS
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('EVENTOS')
+            and   type = 'U')
+   drop table EVENTOS
 go
 
 if exists (select 1
@@ -69,30 +75,9 @@ go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('ALBUNS_EVENTOS')
-            and   type = 'U')
-   drop table ALBUNS_EVENTOS
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('EVENTOS')
-            and   type = 'U')
-   drop table EVENTOS
-go
-
-if exists (select 1
-            from  sysobjects
            where  id = object_id('SUB_AREA_ATUACAO')
             and   type = 'U')
    drop table SUB_AREA_ATUACAO
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('ALBUNSMOMENTOS')
-            and   type = 'U')
-   drop table ALBUNSMOMENTOS
 go
 
 if exists (select 1
@@ -145,7 +130,7 @@ create table UTILIZADORES (
    DESCRICAO            text                 null,
    MORADA               text                 null,
    DATANASCIMENTO       datetime             null,
-   DATAREGISTO			datetime             null,
+   DATAREGISTO          datetime             null,
    TELEFONE             int                  null,
    EMAIL                text                 not null,
    PASSWORD             text                 not null,
@@ -185,29 +170,6 @@ create table AREASDEATUACAO (
 go
 
 /*==============================================================*/
-/* Table: ALBUNSMOMENTOS                                        */
-/*==============================================================*/
-create table ALBUNSMOMENTOS (
-   ID_ALBUM             int                  not null,
-   ID_AREA              int                  not null,
-   ID_UTILIZADOR        int                  not null,
-   ID_CENTRO            int                  not null,
-   NOME                 text                 not null,
-   IMAGEM               image                null,
-   DATA_CRIACAO         datetime             null,
-   LOCALIZACAO          text                 null,
-   VISIBILIDADE         int                  null,
-   constraint PK_ALBUNSMOMENTOS primary key (ID_ALBUM),
-   constraint FK_ALBUNSMO_ALBUNS_DA_AREASDEA foreign key (ID_AREA)
-      references AREASDEATUACAO (ID_AREA),
-   constraint FK_ALBUNSMO_U_CRIA_AL_UTILIZAD foreign key (ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR),
-   constraint FK_ALBUNSMO_ALBUNS_CE_CENTRO foreign key (ID_CENTRO)
-      references CENTRO (ID_CENTRO)
-)
-go
-
-/*==============================================================*/
 /* Table: SUB_AREA_ATUACAO                                      */
 /*==============================================================*/
 create table SUB_AREA_ATUACAO (
@@ -219,57 +181,6 @@ create table SUB_AREA_ATUACAO (
    constraint PK_SUB_AREA_ATUACAO primary key (ID_SUBAREA),
    constraint FK_SUB_AREA_SUBAREA_A_AREASDEA foreign key (ID_AREA)
       references AREASDEATUACAO (ID_AREA)
-)
-go
-
-/*==============================================================*/
-/* Table: EVENTOS                                               */
-/*==============================================================*/
-create table EVENTOS (
-   ID_EVENTO            int                  not null,
-   ID_SUBAREA           int                  not null,
-   ID_UTILIZADOR        int                  not null,
-   ID_CENTRO            int                  not null,
-   ID_AREA              int                  not null,
-   UTI_ID_UTILIZADOR    int                  not null,
-   NOME                 text                 not null,
-   DATA                 datetime             not null,
-   LOCALIZACAO          text                 not null,
-   TELEFONE             int                  null,
-   DESCRICAO            text                 null,
-   PRECO                numeric              not null,
-   REVISTO              bit                  null,
-   constraint PK_EVENTOS primary key (ID_EVENTO),
-   constraint FK_EVENTOS_EVENTOS_P_SUB_AREA foreign key (ID_SUBAREA)
-      references SUB_AREA_ATUACAO (ID_SUBAREA),
-   constraint FK_EVENTOS_ADMIN_REV_UTILIZAD foreign key (UTI_ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR),
-   constraint FK_EVENTOS_EVENTOS_C_CENTRO foreign key (ID_CENTRO)
-      references CENTRO (ID_CENTRO),
-   constraint FK_EVENTOS_AREA_EVEN_AREASDEA foreign key (ID_AREA)
-      references AREASDEATUACAO (ID_AREA),
-   constraint FK_EVENTOS_RELATIONS_UTILIZAD foreign key (ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR)
-)
-go
-
-/*==============================================================*/
-/* Table: ALBUNS_EVENTOS                                        */
-/*==============================================================*/
-create table ALBUNS_EVENTOS (
-   ID_ALBUMEVENTO       int                  not null,
-   ID_EVENTO            int                  not null,
-   ID_UTILIZADOR        int                  null,
-   NOME                 text                 not null,
-   IMAGEM               image                null,
-   DATA_CRIACAO         datetime             null,
-   LOCALIZACAO          text                 null,
-   VISIBILIDADE         int                  null,
-   constraint PK_ALBUNS_EVENTOS primary key (ID_ALBUMEVENTO),
-   constraint FK_ALBUNS_E_EVENTOS_A_EVENTOS foreign key (ID_EVENTO)
-      references EVENTOS (ID_EVENTO),
-   constraint FK_ALBUNS_E_U_CRIA_AL_UTILIZAD foreign key (ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR)
 )
 go
 
@@ -287,9 +198,10 @@ create table CONTEUDO (
    MORADA               text                 not null,
    HORARIO              text                 not null,
    TELEFONE             int                  not null,
+   IMAGEMCONTEUDO       image                null,
    WEBSITE              text                 null,
    ACESSIBILIDADE       text                 null,
-   REVISTO              bit                  null,
+   REVISTO              bit                  null default 0,
    constraint PK_CONTEUDO primary key (ID_CONTEUDO),
    constraint FK_CONTEUDO_CONTEM_SUB_AREA foreign key (ID_SUBAREA)
       references SUB_AREA_ATUACAO (ID_SUBAREA),
@@ -342,11 +254,45 @@ create table DOCUMENTOS (
 go
 
 /*==============================================================*/
+/* Table: EVENTOS                                               */
+/*==============================================================*/
+create table EVENTOS (
+   ID_EVENTO            int                  not null,
+   ID_SUBAREA           int                  not null,
+   ID_UTILIZADOR        int                  not null,
+   ID_CENTRO            int                  not null,
+   ID_AREA              int                  not null,
+   UTI_ID_UTILIZADOR    int                  not null,
+   NOME                 text                 not null,
+   DATA                 datetime             not null,
+   LOCALIZACAO          text                 not null,
+   IMAGEMEVENTO         image                null,
+   TELEFONE             int                  null,
+   DESCRICAO            text                 null,
+   PRECO                decimal              not null,
+   REVISTO              bit                  null default 0,
+   constraint PK_EVENTOS primary key (ID_EVENTO),
+   constraint FK_EVENTOS_EVENTOS_P_SUB_AREA foreign key (ID_SUBAREA)
+      references SUB_AREA_ATUACAO (ID_SUBAREA),
+   constraint FK_EVENTOS_ADMIN_REV_UTILIZAD foreign key (UTI_ID_UTILIZADOR)
+      references UTILIZADORES (ID_UTILIZADOR),
+   constraint FK_EVENTOS_EVENTOS_C_CENTRO foreign key (ID_CENTRO)
+      references CENTRO (ID_CENTRO),
+   constraint FK_EVENTOS_AREA_EVEN_AREASDEA foreign key (ID_AREA)
+      references AREASDEATUACAO (ID_AREA),
+   constraint FK_EVENTOS_RELATIONS_UTILIZAD foreign key (ID_UTILIZADOR)
+      references UTILIZADORES (ID_UTILIZADOR)
+)
+go
+
+/*==============================================================*/
 /* Table: FAVORITOS                                             */
 /*==============================================================*/
 create table FAVORITOS (
    IDFAVORITO           int                  not null,
+   ID_AREA              int                  not null,
    ID_CENTRO            int                  not null,
+   ID_SUBAREA           int                  not null,
    ID_CONTEUDO          int                  not null,
    ID_UTILIZADOR        int                  not null,
    DATAFAVORITO         datetime             null,
@@ -356,27 +302,31 @@ create table FAVORITOS (
    constraint FK_FAVORITO_RELATIONS_CONTEUDO foreign key (ID_CONTEUDO)
       references CONTEUDO (ID_CONTEUDO),
    constraint FK_FAVORITO_RELATIONS_CENTRO foreign key (ID_CENTRO)
-      references CENTRO (ID_CENTRO)
+      references CENTRO (ID_CENTRO),
+   constraint FK_FAVORITO_RELATIONS_SUB_AREA foreign key (ID_SUBAREA)
+      references SUB_AREA_ATUACAO (ID_SUBAREA),
+   constraint FK_FAVORITO_RELATIONS_AREASDEA foreign key (ID_AREA)
+      references AREASDEATUACAO (ID_AREA)
 )
 go
 
 /*==============================================================*/
-/* Table: FOTOGRAFIAS                                           */
+/* Table: FOTOGRAFIAS_CONTEUDO                                  */
 /*==============================================================*/
-create table FOTOGRAFIAS (
+create table FOTOGRAFIAS_CONTEUDO (
    ID_FOTO              int                  not null,
+   ID_CONTEUDO          int                  not null,
    ID_UTILIZADOR        int                  not null,
-   ID_ALBUM             int                  not null,
    DESCRICAO            text                 null,
    LOCALIZACAO          text                 null,
    DATA_CRIACAO         datetime             null,
    IMAGEM               image                null,
-   VISIBILIDADE         int                  null,
-   constraint PK_FOTOGRAFIAS primary key (ID_FOTO),
-   constraint FK_FOTOGRAF_FOTOS_ALB_ALBUNSMO foreign key (ID_ALBUM)
-      references ALBUNSMOMENTOS (ID_ALBUM),
+   VISIBILIDADE         bit                  null default 1,
+   constraint PK_FOTOGRAFIAS_CONTEUDO primary key (ID_FOTO),
    constraint FK_FOTOGRAF_U_ADD_FOT_UTILIZAD foreign key (ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR)
+      references UTILIZADORES (ID_UTILIZADOR),
+   constraint FK_FOTOGRAF_RELATIONS_CONTEUDO foreign key (ID_CONTEUDO)
+      references CONTEUDO (ID_CONTEUDO)
 )
 go
 
@@ -385,18 +335,18 @@ go
 /*==============================================================*/
 create table FOTOGRAFIAS_EVENTOS (
    ID_FOTO2             int                  not null,
-   ID_ALBUMEVENTO       int                  not null,
+   ID_EVENTO            int                  not null,
    ID_UTILIZADOR        int                  not null,
    DESCRICAO            text                 null,
    LOCALIZACAO          text                 null,
    DATA_CRIACAO         datetime             null,
    IMAGEM               image                null,
-   VISIBILIDADE         int                  null,
+   VISIBILIDADE         bit                  null default 1,
    constraint PK_FOTOGRAFIAS_EVENTOS primary key (ID_FOTO2),
-   constraint FK_FOTOGRAF_RELATIONS_ALBUNS_E foreign key (ID_ALBUMEVENTO)
-      references ALBUNS_EVENTOS (ID_ALBUMEVENTO),
    constraint FK_FOTOGRAF_RELATIONS_UTILIZAD foreign key (ID_UTILIZADOR)
-      references UTILIZADORES (ID_UTILIZADOR)
+      references UTILIZADORES (ID_UTILIZADOR),
+   constraint FK_FOTOGRAF_RELATIONS_EVENTOS foreign key (ID_EVENTO)
+      references EVENTOS (ID_EVENTO)
 )
 go
 
