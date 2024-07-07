58f2d59b197044174e656d00b48f3b02bf32f15a
Kikoszzz <pv25219@alunos.estgv.ipv.pt>
2024-07-07 16:29:36 +0100

    Regisot na DB com Google

--- PDM/soft_shares/lib/services/auth_service.dart
+++ PDM/soft_shares/lib/services/auth_service.dart
@@ -3,6 +3,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../database/server.dart';
+import '../database/var.dart' as globals;
+
 class AuthService {
 
   // Google Sign In
@@ -34,6 +37,21 @@ class AuthService {
 
       if (user != null) {
         // Login bem-sucedido
+        print('User ID: ${user.uid}');
+        print('User Name: ${user.displayName}');
+        print('User Email: ${user.email}');
+        print('User Photo URL: ${user.photoURL}');
+        print('Email Verificado: ${user.emailVerified}');
+        print('Número de Telefone: ${user.phoneNumber}');
+        print('Login Anônimo: ${user.isAnonymous}');
+        print('Metadata (Criação): ${user.metadata.creationTime}');
+        print('Metadata (Último Login): ${user.metadata.lastSignInTime}');
+        print('Provedores: ${user.providerData}');
+        print('ID do Locatário: ${user.tenantId}');
+        print('Token de Atualização: ${user.refreshToken}');
+
+        await registo(globals.idCentro, user.displayName, user.email, user.uid);
+
         return true;
       } else {
         // Falha no login
