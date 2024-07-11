8017a3087c87cf3aa7faac89fd8a498fcce607e5
Kikoszzz <pv25219@alunos.estgv.ipv.pt>
2024-07-11 17:14:46 +0100

    Snall fix

--- PDM/soft_shares/lib/conteudo.dart
+++ PDM/soft_shares/lib/conteudo.dart
@@ -211,12 +211,12 @@ class Conteudo extends StatelessWidget {
                                     ),
                                     const SizedBox(width: 5),
                                     RatingBarIndicator(
-                                      rating: ratingPreco.toDouble(),
+                                      rating: ratingEstrela.toDouble(),
                                       itemBuilder: (context, index) => const Icon(
                                         Icons.star,
                                         color: Color.fromARGB(0xFF, 0x00, 0xB8, 0xE0),
                                       ),
-                                      itemCount: 3,
+                                      itemCount: 5,
                                       itemSize: 20.0,
                                       direction: Axis.horizontal,
                                     ),
