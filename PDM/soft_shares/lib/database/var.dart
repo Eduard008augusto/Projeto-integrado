04d06b8bb85a1b7643001ec5dcbcb956fdf167d8
Kikoszzz <pv25219@alunos.estgv.ipv.pt>
2024-08-19 14:53:32 +0100

    Small fix

--- PDM/soft_shares/lib/database/var.dart
+++ PDM/soft_shares/lib/database/var.dart
@@ -1,6 +1,6 @@
 library globals;
 
-var idCentro = 1;
+var idCentro = 0;
 var idArea = 0;
 var idSubArea = 0;
 
--- PDM/soft_shares/lib/mapa.dart
+++ PDM/soft_shares/lib/mapa.dart
@@ -33,7 +33,7 @@ class Mapa extends StatelessWidget {
           ),
         ),
 
-      drawer: const MenuDrawer(),
+      drawer: const MenuDrawerMapa(),
       body: Center(
         child: Stack(
           children: [
