95c76e34609fc2097ca08014d6a8c69ebbc73944
Kikoszzz <pv25219@alunos.estgv.ipv.pt>
2024-08-27 15:07:57 +0100

    Small fix

--- PDM/soft_shares/lib/conteudo_to_edit.dart
+++ PDM/soft_shares/lib/conteudo_to_edit.dart
@@ -6,38 +6,11 @@ import 'package:soft_shares/drawer.dart';
 import './database/var.dart' as globals;
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-bool avaliado = false;
-
-int estrela = 0;
-int preco = 0;
-
-int estrelaBD = 0;
-int precoBD = 0;
-
 class ConteudoToEdit extends StatelessWidget {
   const ConteudoToEdit({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future<void> check() async {
-      try {
-        var data = await checkAvaliacao(globals.idUtilizador, globals.idPublicacao);
-        if (data['Avaliou']) {
-          avaliado = true;
-          Map<String, dynamic> res = Map<String, dynamic>.from(data['avaliacao']);
-          estrela = res['AVALIACAOGERAL']!;
-          print(estrela);
-          preco = res['AVALIACAOPRECO'];
-        } else {
-          avaliado = false;
-        }
-      } catch (e) {
-        print('Erro ao verificar avaliação: $e');
-      }
-    }
-
-    check();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -301,58 +274,6 @@ class ConteudoToEdit extends StatelessWidget {
           child: const Icon(Icons.edit, color: Colors.white),
         ),
       ),
-    );
-  }
-}
- 
-class CustomStarRating extends StatelessWidget {
-  final int rating;
-
-  const CustomStarRating({required this.rating, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return RatingBar.builder(
-      initialRating: rating.toDouble(),
-      minRating: 1,
-      direction: Axis.horizontal,
-      itemCount: 5,
-      itemSize: 20.0,
-      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-      itemBuilder: (context, _) => const Icon(
-        Icons.star,
-        color: Color.fromARGB(0xFF, 0x00, 0xB8, 0xE0),
-      ),
-      unratedColor: Colors.grey.withOpacity(0.5),
-      onRatingUpdate: (rating) {
-        estrelaBD = rating.toInt();
-      },
-    );
-  }
-}
-
-class CustomEuroRating extends StatelessWidget {
-  final int rating;
-
-  const CustomEuroRating({required this.rating, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return RatingBar.builder(
-      initialRating: rating.toDouble(),
-      minRating: 1,
-      direction: Axis.horizontal,
-      itemCount: 3,
-      itemSize: 20.0,
-      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-      itemBuilder: (context, _) => const Icon(
-        Icons.euro,
-        color: Colors.black,
-      ),
-      unratedColor: Colors.black.withOpacity(0.3),
-      onRatingUpdate: (rating) {
-        precoBD = rating.toInt();
-      },
     );
   }
 }
